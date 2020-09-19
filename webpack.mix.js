0c607fa7b69b669ef44dae7bd2943d085e83afb0
Krzysztof Bielikowicz <kbielikowicz98@gmail.com>
2020-09-19 13:51:21 +0100

    Quick fix - added classes to whitelist for purge

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,9 @@
-const cssImport = require("postcss-import");
-const cssNesting = require("postcss-nesting");
-const mix = require("laravel-mix");
-const path = require("path");
-const purgecss = require("@fullhuman/postcss-purgecss");
-const tailwindcss = require("tailwindcss");
+const cssImport = require('postcss-import');
+const cssNesting = require('postcss-nesting');
+const mix = require('laravel-mix');
+const path = require('path');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -16,35 +16,36 @@ const tailwindcss = require("tailwindcss");
  |
  */
 
-mix.js("resources/js/app.js", "public/js")
-    .postCss("resources/css/app.css", "public/css/app.css")
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css/app.css')
     .options({
         postCss: [
             cssImport(),
             cssNesting(),
-            tailwindcss("tailwind.config.js"),
+            tailwindcss('tailwind.config.js'),
             ...(mix.inProduction()
                 ? [
                       purgecss({
                           content: [
-                              "./resources/views/**/*.blade.php",
-                              "./resources/js/**/*.vue"
+                              './resources/views/**/*.blade.php',
+                              './resources/js/**/*.vue',
                           ],
                           defaultExtractor: content =>
                               content.match(/[\w-/:.]+(?<!:)/g) || [],
-                          whitelistPatternsChildren: [/nprogress/]
-                      })
+                          whitelistPatternsChildren: [/nprogress/, /tooltip/],
+                          whitelist: ['w-1/2', 'w-1/4', 'w-2/4', 'w-20'],
+                      }),
                   ]
-                : [])
-        ]
+                : []),
+        ],
     })
     .webpackConfig({
-        output: { chunkFilename: "js/[name].js?id=[chunkhash]" },
+        output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
         resolve: {
             alias: {
-                "@": path.resolve("resources/js")
-            }
-        }
+                '@': path.resolve('resources/js'),
+            },
+        },
     })
     .version()
     .sourceMaps();
